c5f528022fe9fd51514322b9bc329944bf9fbae0
amshirazi64 <amshirazi64@gmail.com>
2023-11-20 12:45:19 +0330

    domain package(Class Author,Class Book), jpa-entities, jpa-relationships

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spring-6-webapp
+Spring 6 Web App
--- src/main/java/springframwork/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/springframwork/spring6webapp/Spring6WebappApplication.java
@@ -1,4 +1,4 @@
-package amsh.springframwork.spring6webapp;
+package springframwork.spring6webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
